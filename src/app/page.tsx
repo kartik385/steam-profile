03091bd1b57c078da5402b74f3cbb0422a13ea91
kartik385 <kartikb385@gmail.com>
2025-01-09 00:51:00 +0530

    Add utility functions and update layout for SteamScope application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,32 @@
-interface PlayedGameResponse {
-  response: Response;
-}
+import { Spotlight } from "./components/ui/Spotlight";
 
-interface Response {
-  total_count: number;
-  games: Game[];
-}
-
-interface Game {
-  appid: number;
-  name: string;
-  playtime_2weeks: number;
-  playtime_forever: number;
-  img_icon_url: string;
-  playtime_windows_forever: number;
-  playtime_mac_forever: number;
-  playtime_linux_forever: number;
-  playtime_deck_forever: number;
-}
 export default async function Home() {
-  const recetlyPlayedGames = await fetch(
-    `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1?key=${process.env.STEAM_KEY}&steamid=${process.env.DEFAULT_USER}&format=json`
-  );
-  const recetlyPlayedGamesJson: PlayedGameResponse =
-    await recetlyPlayedGames.json();
   return (
-    <div>
-      <h1>Recently Played Games</h1>
-      <ul>
-        {recetlyPlayedGamesJson.response.games.map((game) => (
-          <li key={game.appid}>
-            <img
-              src={`https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`}
-              alt={game.name}
-            />
-            <h2>{game.name}</h2>
-            <p>Playtime: {game.playtime_2weeks} minutes</p>
-          </li>
-        ))}
-      </ul>
+    <div className="h-screen w-full rounded-md flex items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          SteamScope
+        </h1>
+        <p className="mt-7 font-normal text-lg text-neutral-300 max-w-lg text-center mx-auto">
+          Your complete Steam profile analyzer. Dive deep into your gaming
+          journey with detailed statistics and insights.
+        </p>
+        <div className="mt-10 flex justify-center">
+          <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+            <span className="absolute inset-0 overflow-hidden rounded-full">
+              <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+            </span>
+            <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-1 px-10 ring-1 ring-white/10 text-base font-bold ">
+              Login with Steam
+            </div>
+            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
