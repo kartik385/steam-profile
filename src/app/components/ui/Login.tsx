a5cc9faaca285817470f67c88311fa4dca57d14e
kartik385 <kartikb385@gmail.com>
2025-01-15 01:59:07 +0530

    Add QR code authentication and loading skeleton to Login component

--- src/app/components/ui/Login.tsx
+++ src/app/components/ui/Login.tsx
@@ -2,55 +2,69 @@
 
 import React, { useActionState } from "react";
 import { BackgroundBeams } from "./background-beams";
-// import useSWR from "swr";
-// import { qrFetcher } from "@/app/lib/apis";
-// import SkeletonLoader from "./skeletonLoader";
-// import QRCode from "react-qr-code";
+import useSWR from "swr";
+import { qrFetcher, qrPoller } from "@/app/lib/apis";
+import SkeletonLoader from "./skeletonLoader";
+import QRCode from "react-qr-code";
 import { loginUser } from "@/app/lib/actions";
 
 function Login() {
-  // const { data, isLoading } = useSWR(
-  //   "IAuthenticationService/BeginAuthSessionViaQR/v1/",
-  //   qrFetcher
-  // );
+  const { data: QrData, isLoading } = useSWR(
+    "IAuthenticationService/BeginAuthSessionViaQR/v1/",
+    qrFetcher
+  );
+
+  if (QrData?.response) {
+    qrPoller(QrData.response.request_id, +QrData.response.client_id);
+  }
 
   const [data, action, pending] = useActionState(loginUser, undefined);
 
   return (
     <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-      <div className="max-w-2xl mx-auto p-4">
-        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          Join the waitlist
+      <div className="max-w-3xl mx-auto p-4">
+        <h1 className="relative z-10 text-3xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          Login
         </h1>
-        <p></p>
-        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-          Welcome to MailJet, the best transactional email service on the web.
-          We provide reliable, scalable, and customizable email solutions for
-          your business. Whether you&apos;re sending order confirmations,
-          password reset emails, or promotional campaigns, MailJet has got you
-          covered.
-        </p>
-        <form action={action}>
-          <input
-            type="text"
-            placeholder="steam id"
-            id="steam-id"
-            name="steam-id"
-            autoFocus
-            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
-          />
-          <button
-            type="submit"
-            disabled={pending}
-            className="bg-teal-500 text-neutral-950 w-full rounded-lg p-2 mt-4 relative z-10"
-          >
-            Login
-          </button>
 
-          {data?.error && (
-            <p className="text-red-500 text-sm mt-2">{data.error}</p>
-          )}
-        </form>
+        <div className="flex justify-between items-center mt-5 gap-20 md:divide-x md:divide-gray-600  ">
+          <div>
+            <form action={action}>
+              <input
+                type="text"
+                placeholder="Enter You Steam ID"
+                id="steam-id"
+                name="steam-id"
+                autoFocus
+                className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+              />
+              <button
+                type="submit"
+                disabled={pending}
+                className="bg-teal-500 text-neutral-950 w-full rounded-lg p-2 mt-4 relative z-10"
+              >
+                Login
+              </button>
+
+              {data?.error && (
+                <p className="text-red-500 text-sm mt-2">{data.error}</p>
+              )}
+            </form>
+          </div>
+
+          <div className="hidden md:block pl-20">
+            <div>
+              {isLoading ? (
+                <SkeletonLoader />
+              ) : (
+                <QRCode
+                  value={QrData?.response.challenge_url || ""}
+                  size={150}
+                />
+              )}
+            </div>
+          </div>
+        </div>
       </div>
       <BackgroundBeams />
     </div>
--- src/app/lib/apis.ts
+++ src/app/lib/apis.ts
@@ -23,3 +23,19 @@ export const qrFetcher = (url: string) =>
       device_details: "This a is web",
     })
     .then((res) => res.data);
+
+export const qrPoller = async (request_id: string, client_id: number) => {
+  try {
+    const reponse = await axiosInstance.post<QRResponse>(
+      "IAuthenticationService/PollAuthSessionStatus/v1/",
+      {
+        request_id: request_id,
+        client_id: client_id,
+        token_to_revoke: "",
+      }
+    );
+    console.log(reponse.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
